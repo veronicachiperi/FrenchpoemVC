43d272af5afbe021822b37ccc6acc1cbd2f7853d
veronicachiperi <144971814+veronicachiperi@users.noreply.github.com>
2024-07-13 06:34:28 -0400

    API Key

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function generatePoem(event) {
   let context =
     "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
-  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={key}`;
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
