038449af0506a28269543f39241e20b62ff67539
veronicachiperi <144971814+veronicachiperi@users.noreply.github.com>
2024-07-14 15:08:27 -0400

    index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function displayPoem(response) {
+function displayProgram(response) {
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -10,21 +10,21 @@ function displayPoem(response) {
 function generatePoem(event) {
   event.preventDefault();
 
-  let instructionsInput = document.querySelector("#user-instructions").value.trim();  // Get the value and trim it
+  let instructionsInput = document.querySelector("#user-instructions").value.trim(); 
   let apiKey = "01f7b532710b4e09a2cat93a396e1ao1";
-  let context = "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
-  let prompt = `User instructions: Generate a French poem about ${instructionsInput}`;
+  let context = "You are an expert in nutrition. Your mission is to generate 1 week nutrition programs based on user weight in basic HTML and separate each line with a <br />. Make sure to follow the user weight. Do not include a title to the program. Sign the program with 'VC' inside a <strong> element at the end of the program and NOT at the beginning";
+  let prompt = `User instructions: Generate a nutrition program after reading ${instructionsInput}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(prompt)}&context=${encodeURIComponent(context)}&key=${apiKey}`;
 
-  let poemElement = document.querySelector("#poem");
-  poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput}</div>`;
+  let programElement = document.querySelector("#program");
+  programElement.classList.remove("hidden");
+  programElement.innerHTML = `<div class="generating">⏳ Generating your Nutrition program based on ${instructionsInput}</div>`;
 
   axios.get(apiURL).then(displayPoem).catch(error => {
     console.error("Error generating poem:", error);
-    poemElement.innerHTML = `<div class="error">❌ Error generating poem. Please try again later.</div>`;
+    programElement.innerHTML = `<div class="error">❌ Error generating program. Please try again later.</div>`;
   });
 }
 
-let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatePoem);
+let programFormElement = document.querySelector("#program-generator-form");
+programFormElement.addEventListener("submit", generateProgram);
