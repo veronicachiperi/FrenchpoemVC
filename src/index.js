3e92462d9c1a69b26a387e1d4fe8ae46f87cb868
veronicachiperi <144971814+veronicachiperi@users.noreply.github.com>
2024-07-15 04:07:48 -0400

    updates

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function generateProgram(event) {
 
   let instructionsInput = document.querySelector("#user-instructions").value.trim(); 
   let apiKey = "01f7b532710b4e09a2cat93a396e1ao1";
-  let context = "You are an expert in nutrition. Your mission is to generate 1 week nutrition programs based on user weight in basic HTML table, separating each day of the week. Make sure to follow the user weight. Do not include a title to the program. Sign the program with 'VC' inside a <strong> element at the end of the program and NOT at the beginning";
+  let context = "You are an expert in nutrition. Your mission is to generate 3 day nutrition programs based on user weight in basic HTML, separating each day by <br>. Make sure to follow the user weight. Do not include a title to the program. Sign the program with 'VC' inside a <strong> element at the end of the program and NOT at the beginning";
   let prompt = `User instructions: Generate a nutrition program after reading ${instructionsInput}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(prompt)}&context=${encodeURIComponent(context)}&key=${apiKey}`;
 
